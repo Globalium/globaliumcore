7abd59b6c24a2ab45ab99e3212f9a93e36e10e6b
Diego Maroto <diego.diemg@gmail.com>
2017-10-19 13:52:35 +0200

    refactor struct and main file

--- src/globaliumcore/main.go
+++ src/globaliumcore/main.go
@@ -3,63 +3,13 @@ package main
 import(
 	glo "glostructs"
 	"fmt"
-	"crypto/rand"
-    "crypto/rsa"
-    "crypto/x509"
-	"encoding/pem"
-	"errors"
 )
-var version string = "2017.0.1.0"
-
-func PrivateKeyToEncryptedPEM(bits int, pwd string) ([]byte, error) {
-	if pwd == ""{
-		err := errors.New("Cannot use an empty password.")
-		return nil,err
-	}
-    // Generate the key of length bits
-    key, err := rsa.GenerateKey(rand.Reader, bits)
-    if err != nil {
-        return nil, err
-	}
-	fmt.Println(key)
-
-    // Convert it to pem
-    block := &pem.Block{
-        Type:  "RSA PRIVATE KEY",
-        Bytes: x509.MarshalPKCS1PrivateKey(key),
-	}
-	
-
-	fmt.Println("OK--------------------")
-    // Encrypt the pem
-	block, err = x509.EncryptPEMBlock(rand.Reader, block.Type, block.Bytes, []byte(pwd), x509.PEMCipherAES256)
-	if err != nil {
-		return nil, err
-	}
-	der, err := x509.DecryptPEMBlock(block, []byte(pwd))
-	if err != nil {
-		panic("Error decrypt pem block")
-	}
-
-	oldkey, err := x509.ParsePKCS1PrivateKey(der)
-	if err != nil{
-		panic(err)
-	}
-	fmt.Println(oldkey)
-
-    return pem.EncodeToMemory(block), nil
-}
+var version string = "v2017.0.1.0"
 
 func main(){
 	var wallet glo.Wallet
-	fmt.Println("GLOBALIUM 0.1.0")
+	fmt.Println("GLOBALIUM "+version)
 	fmt.Println("Example of empty wallet: ")
 	fmt.Println(wallet)
-	clave := "123456789"
-	pem,err := PrivateKeyToEncryptedPEM(1024,clave)
-	if err != nil{
-		fmt.Println(err)
-		panic("Error generating rsa private key.")
-	}
-	fmt.Println(pem)
+
 }
--- src/glostructs/wallet.go
+++ src/glostructs/wallet.go
@@ -1,6 +1,68 @@
 package glostructs
 
+import(
+	"crypto/rand"
+    "crypto/rsa"
+    "crypto/x509"
+	"encoding/pem"
+)
+
 type Wallet struct{
 	PublicKey [64]byte 	//Public key and account id of the wallet
 	Amount uint64		//Amount of money holded by the account, unsigned int of 8 digits as minimum
 }
+
+func(wallet *Wallet) New(passphrase string)(bool, error){
+	pk, err := wallet.createPrivateKey(2048)
+	if err != nil {
+		return false, err
+	}
+	pem := wallet.getPemFromPK(pk)
+	block, err := wallet.EncryptPEMBlock(pem,passphrase)
+	if err != nil {
+		return false, err
+	}
+	
+	return true,nil
+}
+
+func(wallet *Wallet) createPrivateKey(bits int) (*rsa.PrivateKey, error){
+    pk, err := rsa.GenerateKey(rand.Reader, bits)
+    if err != nil {
+        return nil, err
+	}
+	return pk,nil
+}
+
+func(wallet *Wallet) getPemFromPK(pk *rsa.PrivateKey) (*pem.Block){
+    block := &pem.Block{
+        Type:  "RSA PRIVATE KEY",
+        Bytes: x509.MarshalPKCS1PrivateKey(pk),
+	}
+	return block
+}
+
+func(wallet *Wallet) EncryptPEMBlock(block *pem.Block, passphrase string) (*pem.Block, error){
+	block, err := x509.EncryptPEMBlock(rand.Reader, block.Type, block.Bytes, []byte(passphrase), x509.PEMCipherAES256)
+	if err != nil {
+		return nil, err
+	}
+	return block,nil
+}
+
+func(wallet *Wallet) DecryptPEMBlock(block *pem.Block, passphrase string) ([]byte, error){
+	der, err := x509.DecryptPEMBlock(block, []byte(passphrase))
+	if err != nil {
+		return nil, err
+	}
+	return der,nil
+}
+
+func(wallet *Wallet) getPKFromPem(der []byte) (*rsa.PrivateKey, error){
+	pk, err := x509.ParsePKCS1PrivateKey(der)
+	if err != nil{
+		return nil, err
+	}
+	return pk, nil
+}
+//pem.EncodeToMemory(block), nil
