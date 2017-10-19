14ee5c812ca1196c6756bae55588b4589337d4b8
Diego Maroto <diego.diemg@gmail.com>
2017-10-19 19:15:10 +0200

    Wallet's struct almost done. Contains a test that generates a new wallet printing by screen the wallet struct and private key on pem encryptation for debug purposes

--- src/globaliumcore/main.go
+++ src/globaliumcore/main.go
@@ -3,13 +3,34 @@ package main
 import(
 	glo "glostructs"
 	"fmt"
+	"bufio"
+	"os"
 )
 var version string = "v2017.0.1.0"
 
-func main(){
-	var wallet glo.Wallet
-	fmt.Println("GLOBALIUM "+version)
-	fmt.Println("Example of empty wallet: ")
-	fmt.Println(wallet)
+func menu() (string, error){
+	fmt.Println("What do you want to do?")
+	fmt.Println("\t1.- Test new wallet")
+	in := bufio.NewReader(os.Stdin)
+	opcion, err := in.ReadString('\n')
+	if err != nil {
+		return "",err
+	}
+	opcion = opcion[:len(opcion)-1]
+	return opcion,nil
+}
 
+func main(){
+	fmt.Println("GLOBALIUM "+version+"\n")
+	opcion, err := menu()
+	if err != nil {
+		panic(err)
+	}
+	switch opcion{
+	case "1":
+		fmt.Println("Testing: Create new wallet...")
+		glo.TestNewWallet()
+	default:
+		fmt.Println("Option don't supported.")
+	}
 }
--- src/glostructs/wallet.go
+++ src/glostructs/wallet.go
@@ -1,29 +1,83 @@
 package glostructs
 
 import(
+	"fmt"
+	"os"
+	"bufio"
 	"crypto/rand"
     "crypto/rsa"
     "crypto/x509"
 	"encoding/pem"
+	"encoding/json"
+	"encoding/base64"
+	"crypto/sha256"
 )
 
 type Wallet struct{
-	PublicKey [64]byte 	//Public key and account id of the wallet
-	Amount uint64		//Amount of money holded by the account, unsigned int of 8 digits as minimum
+	Address [32]byte	//Address and id account of the wallet
+	PublicKey rsa.PublicKey	//Public key 
+	Amount uint64	//Amount of money holded by the account, unsigned int of 8 digits as minimum
 }
 
-func(wallet *Wallet) New(passphrase string)(bool, error){
-	pk, err := wallet.createPrivateKey(2048)
+func(wallet *Wallet) New(passphrase string, bits int)(*pem.Block, error){
+	pk, err := wallet.createPrivateKey(bits)
 	if err != nil {
-		return false, err
+		return nil, err
 	}
+	wallet.PublicKey = pk.PublicKey
+	pubjson,err := json.Marshal(pk.PublicKey)
+	wallet.Address = sha256.Sum256(pubjson)
 	pem := wallet.getPemFromPK(pk)
 	block, err := wallet.EncryptPEMBlock(pem,passphrase)
 	if err != nil {
-		return false, err
+		return nil, err
 	}
+	return block,nil
+}
+
+func TestNewWallet(){
+	in := bufio.NewReader(os.Stdin)
+	var wallet Wallet
+	fmt.Println("\n**************************")
+	fmt.Println("IMPORTANT INFO:")
+	fmt.Println("YOUR PASSPHRASE SHOULD BE REMEMBERED, IT ALLOWS YOU TO CERTIFY EVERY TRANSACTION")
+	fmt.Println("AT LAST WE WILL GIVE YOU A PRIVATE KEY, IT SHOULD BE SAVED TO CERTIFY YOUR OWNERSHIP ON WALLET LOGIN")
+	fmt.Println("**************************")
 	
-	return true,nil
+	fmt.Println("\nInput your passphrase (no minimum security implemented yet): ")
+	passphrase,err := in.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	passphrase = passphrase[:len(passphrase)-1]
+	
+	block, err := wallet.New(passphrase,4096)
+	if err != nil{
+		panic(err)
+	}
+	f, err := os.Create("extra/pemblock.txt")
+	if err != nil{
+		panic(err)
+	}
+	blockjson,err := json.Marshal(block)
+	if err != nil{
+		panic(err)
+	}
+	f.WriteString(string(blockjson))
+
+	f2, err := os.Create("extra/pemblockbase64.txt")
+	if err != nil{
+		panic(err)
+	}
+	pembase64 := base64.StdEncoding.EncodeToString(blockjson)
+	f2.WriteString(pembase64)
+	defer f.Close()
+
+	fmt.Println("\nYour wallet is:")
+	fmt.Println(wallet)
+
+	fmt.Println("\nYour private key is:")
+	fmt.Println(pembase64)
 }
 
 func(wallet *Wallet) createPrivateKey(bits int) (*rsa.PrivateKey, error){
